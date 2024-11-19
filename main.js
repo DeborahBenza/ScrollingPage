9ea5d815181f7d6b62f715485da7a2db90bbaa11
hannahholz <h.t.holzweber@gmail.com>
2024-11-19 09:59:52 +0100

    Scrolltrigger for Video
    Scrolltrigger for Video without video itself

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 gsap.registerPlugin(ScrollTrigger)
+
+const bgvideo = document.querySelector('#bgvideo')
+
+bgvideo.pause();
+bgvideo.currentTime = 0;
+
+let sections = gsap.utils.toArry('.step');
+sections.forEach((step, i) => {
+    ScrollTrigger.create({
+        trigger: step,
+        start: 'bottom bottom',
+        end: '+=10000',
+        pin: true,
+        anticipatePin: 1,
+    });
+
+    gsap.fromTo(bgvideo, { currentTime: 3 * i }, {
+        ScrollTrigger: {
+            trigger: step,
+            scrub: 2,
+            start: 'top bottom',
+            end: 'bottom bottom',
+            scrambleText: true,
+        }
+
+    })
+
+})
