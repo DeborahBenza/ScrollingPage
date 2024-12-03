4b8be5c5f268ef8c545517834b38007b61482641
hannahholz <h.t.holzweber@gmail.com>
2024-12-03 09:38:29 +0100

    video,picture and text

--- main.js
+++ main.js
@@ -75,3 +75,54 @@ bgvideo.addEventListener('canplay', function () {
 
 // Ensure image container is visible
 imageContainer.style.display = 'none'; // Initially hide
+
+
+
+gsap.config({ trialWarn: false });
+console.clear();
+gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
+let smoother = ScrollSmoother.create({
+  smooth: 2
+});
+
+let split = new SplitText("h1", { type: "lines" });
+let masks;
+function makeItHappen() {
+  masks = [];
+  split.lines.forEach((target) => {
+    let mask = document.createElement("span");
+    mask.className = "mask";
+    target.append(mask);
+    masks.push(mask);
+    gsap.to(mask, {
+      scaleX: 0,
+      transformOrigin: "right center",
+      ease: "none",
+      scrollTrigger: {
+        trigger: target,
+        markers: {
+          startColor: "white",
+          endColor: "#42a6e0",
+          fontSize: "12px",
+          indent: 10
+        },
+        scrub: true,
+        start: "top center",
+        end: "bottom center"
+      }
+    });
+  });
+}
+
+window.addEventListener("resize", newTriggers);
+
+function newTriggers() {
+  ScrollTrigger.getAll().forEach((trigger, i) => {
+    trigger.kill();
+    masks[i].remove();
+  });
+  split.split();
+  makeItHappen();
+}
+
+makeItHappen();
